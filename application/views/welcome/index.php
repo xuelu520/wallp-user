86110265022d9880e84f51dcfae89b794c723132
xuelu520 <289415287@qq.com>
2015-12-21 23:04:27 +0800

    首页样式更新

--- application/views/welcome/index.php
+++ application/views/welcome/index.php
@@ -14,13 +14,29 @@ include APPPATH."views/common/top.php";
         </div>
     </div>
 </div>
-<div class="row">
+<div class="row" data-wgid="1">
     <div class="container">
         <div class="panel">
             <div class="panel-body">
                 <div class="fl"><h3>次级分类1</h3></div>
                 <div class="fr" style="margin-top: 10px;"><a href="javascript:void(0);">更多</a></div>
             </div>
+        </div>
+        <div>
+            <ul class="img-panel">
+                <li><img src="http://xuelu.b0.upaiyun.com/1/3606b6be5394493feb06587968345e41.jpg!pp"></li>
+                <li><img src="http://xuelu.b0.upaiyun.com/1/3606b6be5394493feb06587968345e41.jpg!pp"></li>
+                <li><img src="http://xuelu.b0.upaiyun.com/1/3606b6be5394493feb06587968345e41.jpg!pp"></li>
+                <li><img src="http://xuelu.b0.upaiyun.com/1/3606b6be5394493feb06587968345e41.jpg!pp"></li>
+                <li><img src="http://xuelu.b0.upaiyun.com/1/3606b6be5394493feb06587968345e41.jpg!pp"></li>
+            </ul>
+            <ul class="img-panel">
+                <li><img src="http://xuelu.b0.upaiyun.com/1/3606b6be5394493feb06587968345e41.jpg!pp"></li>
+                <li><img src="http://xuelu.b0.upaiyun.com/1/3606b6be5394493feb06587968345e41.jpg!pp"></li>
+                <li><img src="http://xuelu.b0.upaiyun.com/1/3606b6be5394493feb06587968345e41.jpg!pp"></li>
+                <li><img src="http://xuelu.b0.upaiyun.com/1/3606b6be5394493feb06587968345e41.jpg!pp"></li>
+                <li><img src="http://xuelu.b0.upaiyun.com/1/3606b6be5394493feb06587968345e41.jpg!pp"></li>
+            </ul>
         </div>
     </div>
 </div>
