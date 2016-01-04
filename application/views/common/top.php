2dd97acca57b253ecf7145a23ea8a1d71bede7b0
xuelu520 <289415287@qq.com>
2016-01-04 23:42:16 +0800

    添加注释

--- application/views/common/top.php
+++ application/views/common/top.php
@@ -13,15 +13,14 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-
     <!-- Site Properities -->
     <title>哇扑-看你所看，想你所想</title>
-
+    <!--公共css文件-->
     <link href="http://cdn.bootcss.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/public/owl-carousel/owl.carousel.css">
     <link rel="stylesheet" href="/public/owl-carousel/owl.theme.css">
     <link href="/public/css/public.css" rel="stylesheet">
-
+    <!--公共JS文件-->
     <script src="http://cdn.bootcss.com/jquery/2.1.4/jquery.min.js"></script>
     <script src="http://cdn.bootcss.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <script src="/public/owl-carousel/owl.carousel.js"></script>
@@ -29,7 +28,7 @@
     <script type="text/javascript" src="http://qzonestyle.gtimg.cn/qzone/openapi/qc_loader.js"
             data-appid="101269058"
             data-redirecturi="http://www.housepig.cn" charset="utf-8"></script>
-    <script>
+    <script type="text/javascript">
         $(function() {
             //初始化QQ登录
             QC.Login({
