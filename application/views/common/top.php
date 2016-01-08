4ea95b8059bb415bb9e6f1cbf386451f58732248
xuelu520 <289415287@qq.com>
2016-01-08 23:48:22 +0800

    QQ 互联 网址验证

--- application/views/common/top.php
+++ application/views/common/top.php
@@ -11,6 +11,7 @@
 <head>
     <!-- Standard Meta -->
     <meta charset="utf-8" />
+    <meta property="qc:admins" content="2454325707607535017636" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <!-- Site Properities -->
