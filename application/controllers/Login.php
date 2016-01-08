dfb8a31cc7a4191496990707be2082bf1e681329
xuelu520 <289415287@qq.com>
2016-01-09 00:09:43 +0800

    QQ回调方法

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -23,10 +23,10 @@ class Login extends CI_Controller {
 
 	//用户点击qq登录按钮调用此函数
 	private function qq_login() {
-		$state = md5(uniqid(rand(), TRUE)); //CSRF protection
+		$_SESSION['login:qq:state'] = md5(uniqid(rand(), TRUE)); //CSRF protection
 		$login_url = "https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id="
 			. QQ_APPID . "&redirect_uri=" . urlencode(QQ_CALLBACK_URL)
-			. "&state=" . $state
+			. "&state=" . $_SESSION['login:qq:state']
 			. "&scope=".QQ_SCOPE;
 		header("Location:$login_url");exit;
 	}
@@ -35,6 +35,81 @@ class Login extends CI_Controller {
 	 * QQ登录回调地址
 	 */
 	public function qq() {
-		echo "<h1>这是QQ登录的回调页面</h1>";exit;
+		echo "<h1>这是QQ登录的回调页面</h1>";
+		$this->qq_callback();
+		$this->get_openid();
+		exit;
+	}
+
+	private function qq_callback()
+	{
+		//debug
+		//print_r($_REQUEST);
+		//print_r($_SESSION);
+		$state = $this->input->get('state',TRUE);
+		$code = $this->input->get('code',TRUE);
+		if($state == $_SESSION['login:qq:state']) //csrf
+		{
+			$token_url = "https://graph.qq.com/oauth2.0/token?grant_type=authorization_code&"
+				. "client_id=" . QQ_APPID . "&redirect_uri=" . urlencode(QQ_CALLBACK_URL)
+				. "&client_secret=" . QQ_APPKEY . "&code=" . $code;
+
+			$response = file_get_contents($token_url);
+			if (strpos($response, "callback") !== false)
+			{
+				$lpos = strpos($response, "(");
+				$rpos = strrpos($response, ")");
+				$response  = substr($response, $lpos + 1, $rpos - $lpos -1);
+				$msg = json_decode($response);
+				if (isset($msg->error))
+				{
+					echo "<h3>error:</h3>" . $msg->error;
+					echo "<h3>msg  :</h3>" . $msg->error_description;
+					exit;
+				}
+			}
+
+			$params = array();
+			parse_str($response, $params);
+
+			//debug
+			print_r($params);
+
+			//set access token to session
+			$_SESSION["access_token"] = $params["access_token"];
+
+		}
+		else
+		{
+			echo("The state does not match. You may be a victim of CSRF.");
+		}
+	}
+
+	private function get_openid()
+	{
+		$graph_url = "https://graph.qq.com/oauth2.0/me?access_token="
+			. $_SESSION['access_token'];
+
+		$str  = file_get_contents($graph_url);
+		if (strpos($str, "callback") !== false)
+		{
+			$lpos = strpos($str, "(");
+			$rpos = strrpos($str, ")");
+			$str  = substr($str, $lpos + 1, $rpos - $lpos -1);
+		}
+
+		$user = json_decode($str);
+		if (isset($user->error))
+		{
+			echo "<h3>error:</h3>" . $user->error;
+			echo "<h3>msg  :</h3>" . $user->error_description;
+			exit;
+		}
+
+		//debug
+		echo("Hello " . $user->openid);
+
+		//set openid to session
+		$_SESSION["openid"] = $user->openid;
 	}
 }
